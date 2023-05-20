eb514bc5439d7adaccce5c8b47446a4dc7418ffa
Jesús <jesus.moya.barajas00@gmail.com>
2023-05-20 19:34:10 +0200

    Guardando subcoleccion y luego recuperarla para probar lo que se va ahcer en inicio

--- app/src/main/java/com/example/urbanfit/ui/bookings/BookingsFragment.kt
+++ app/src/main/java/com/example/urbanfit/ui/bookings/BookingsFragment.kt
@@ -39,43 +39,57 @@ class BookingsFragment : Fragment(), AdapterCallback {
 
         return root
     }
-
+    /**
+     * Inicia la obtencion de datos de la lista a partir del associatedGym del usuario
+     * */
     private fun fixArray() {
-        email = (activity as MainActivity).email
-        db = FirebaseFirestore.getInstance()
+        email = (activity as MainActivity).email // Obtiene el valor de la variable "email" desde la actividad contenedora (MainActivity)
+        db = FirebaseFirestore.getInstance() // Obtiene una instancia de FirebaseFirestore
+
+        // Realiza una consulta a la colección "user" y obtiene el documento correspondiente al email
         db.collection("user").document(email).get().addOnSuccessListener { documentSnapshot ->
-            val associatedGym = documentSnapshot.getString("associatedGym")
+            val associatedGym = documentSnapshot.getString("associatedGym") // Obtiene el valor del campo "associatedGym" del documento
+
+            // Verifica si el campo "associatedGym" no es nulo
             if (associatedGym != null) {
-                getClasses(associatedGym)
+                getClasses(associatedGym) // Llama a la función "getClasses" pasando el valor de "associatedGym" como argumento
             }
         }
     }
 
+    /**
+     * Obtiene los datos de las clases del gimnasio asociado al usuario o al elegido,
+     * los carga en el adaptador para cargar la lista, pero a su vez comprueba si
+     * la hora limite para iniciar la clase hoy para mostrarlas o no
+     * */
     private fun getClasses(associatedGym: String) {
-        db.collection("class").whereEqualTo("associatedGym", "Urban Gran Via").get().addOnSuccessListener { documents ->
+        // Realiza una consulta a la colección "class" donde el campo "associatedGym" sea igual al valor pasado como argumento
+        db.collection("class").whereEqualTo("associatedGym", associatedGym).get().addOnSuccessListener { documents ->
 
             val tempList = mutableListOf<ClassGym>()
             val currentTime = Calendar.getInstance()
 
+            // Itera sobre los documentos obtenidos en la consulta
             for (document in documents) {
-                val timestamp = document.getTimestamp("hour")
+                val timestamp = document.getTimestamp("hour") // Obtiene el valor del campo "hour" como Timestamp
 
-                // Verificar si el campo 'hours' es un valor válido
+                // Verificar si el campo 'hour' es un valor válido (no nulo)
                 if (timestamp != null) {
                     val classTime = Calendar.getInstance()
-                    classTime.time = timestamp.toDate()
+                    classTime.time = timestamp.toDate() // Convierte el Timestamp a Date y establece la fecha en classTime
 
                     // Establecer la fecha actual solo para la comparación de horas y minutos
                     classTime.set(Calendar.YEAR, currentTime.get(Calendar.YEAR))
                     classTime.set(Calendar.MONTH, currentTime.get(Calendar.MONTH))
                     classTime.set(Calendar.DAY_OF_MONTH, currentTime.get(Calendar.DAY_OF_MONTH))
 
-                    // Comparar hora y minutos de la clase
+                    // Comparar hora y minutos de la clase con la hora y minutos actuales
                     val classHour = classTime.get(Calendar.HOUR_OF_DAY)
                     val classMinute = classTime.get(Calendar.MINUTE)
                     val currentHour = currentTime.get(Calendar.HOUR_OF_DAY)
                     val currentMinute = currentTime.get(Calendar.MINUTE)
 
+                    // Verificar si la clase está programada para una hora posterior a la hora actual
                     if (classHour > currentHour || (classHour == currentHour && classMinute > currentMinute)) {
                         val id = document.id
                         val name = document.getString("name") ?: ""
@@ -86,75 +100,87 @@ class BookingsFragment : Fragment(), AdapterCallback {
                         val difficulty = document.getString("difficulty") ?: ""
                         val monitor = document.getString("monitor") ?: ""
 
-                        tempList += ClassGym(
-                            id = id,
-                            name = name,
-                            description = description,
-                            schedule = schedule,
-                            maximumCapacity = maximumCapacity,
-                            capacity = capacity,
-                            difficulty = difficulty,
-                            associatedGym = associatedGym,
-                            monitor = monitor
-                        )
+                        if(capacity<maximumCapacity) {//comprueba que la capacidad no es la misma que la maxima
+
+                            // Agregar la clase a la lista tempList
+                            tempList += ClassGym(
+                                id = id,
+                                name = name,
+                                description = description,
+                                schedule = schedule,
+                                maximumCapacity = maximumCapacity,
+                                capacity = capacity,
+                                difficulty = difficulty,
+                                associatedGym = associatedGym,
+                                monitor = monitor
+                            )
+                        }
                     }
                 }
             }
 
+            // Crear un adaptador personalizado y establecerlo en el RecyclerView
             val adapterClass = AdapterClass(requireContext(), R.layout.bookings_item, tempList)
             adapterClass.setAdapterCallback(this)
             binding.listClass.adapter = adapterClass
-
         }
     }
 
-
-
-
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
+    /**
+     * Metodo sobrecargado a partir de la interfaz proporcionada para pasar la clase
+     * seleccionada en la clase donde decidimos que hacer con el, en este caso una ventana
+     * de seleccion que permite agregar reservas al usuario y guardarlas en la base de datos
+     * */
     override fun onItemClicked(data: ClassGym) {
-        var name=capitalizeFirstLetter(data.name)
+        // Capitalizar el nombre de la clase
+        var name = capitalizeFirstLetter(data.name)
+
+        // Crear un AlertDialog para mostrar la confirmación de reserva
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Confirmación")
         builder.setMessage("¿Deseas reservar la $name?")
         builder.setPositiveButton("Reservar") { dialog, which ->
             // Acciones a realizar cuando se presiona el botón Aceptar
+
+            // Obtener una instancia de FirebaseFirestore
             db = FirebaseFirestore.getInstance()
 
+            // Obtener la fecha actual sin incluir la hora
             val currentDate = Calendar.getInstance().apply {
                 set(Calendar.HOUR_OF_DAY, 0) // Establecer la hora en 00:00
                 set(Calendar.MINUTE, 0)
                 set(Calendar.SECOND, 0)
                 set(Calendar.MILLISECOND, 0)
             }.time
+
+            // Obtener la referencia a la subcolección "booking" del usuario actual
             val userRef = db.collection("user").document(email).collection("booking")
+
+            // Crear un mapa con los datos de la reserva
             val subcollectionData = hashMapOf(
                 "hour" to data.schedule,
                 "date" to currentDate,
                 "class" to data.name,
                 "associatedGym" to data.associatedGym
             )
+
+            // Crear una consulta para verificar si ya existe una reserva con los mismos datos
             val query = userRef.whereEqualTo("date", currentDate)
                 .whereEqualTo("class", data.name)
                 .whereEqualTo("associatedGym", data.associatedGym)
 
+            // Ejecutar la consulta
             query.get()
                 .addOnSuccessListener { documents ->
                     if (!documents.isEmpty) {
                         // Ya existe una reserva para la fecha de hoy y coinciden todos los datos
                         seeMessageRepeatReservationShow("Ya tienes una reserva para esta clase")
                     } else {
-                        // No hay reserva existente, agregar los datos a la subcolección
+                        // No hay reserva existente, agregar los datos a la subcolección "booking"
                         userRef.add(subcollectionData)
                             .addOnSuccessListener { documentReference ->
                                 seeMessageRepeatReservationShow("Se añadió la reserva")
-                                incrementCapacity(data.id)
+                                incrementCapacity(data.id) // Incrementar la capacidad de la clase
                             }
                             .addOnFailureListener { exception ->
                                 seeMessageRepeatReservationShow("No se pudo añadir la reserva")
@@ -166,52 +192,68 @@ class BookingsFragment : Fragment(), AdapterCallback {
                 }
         }
 
+        // Configurar el botón de Cancelar
         builder.setNegativeButton("Cancelar", null)
 
+        // Mostrar el diálogo
         val dialog = builder.create()
         dialog.show()
     }
-    private fun seeMessageRepeatReservationShow(message : String){
+
+    /**
+     * Permite mostrar una alerta con un mensaje informativo especificado por parametro
+     */
+    private fun seeMessageRepeatReservationShow(message: String) {
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Aviso")
         builder.setMessage(message)
 
-// Configurar el botón de aceptar
+        // Configurar el botón de aceptar
         builder.setNegativeButton("Aceptar", null)
 
-// Crear y mostrar la ventana emergente
+        // Crear y mostrar la ventana emergente
         val dialog = builder.create()
         dialog.show()
-
     }
+    /**
+     * Permite incrementar las personas de la clase reservada
+     * */
     fun incrementCapacity(classId: String) {
         Toast.makeText(requireContext(), "$classId", Toast.LENGTH_SHORT).show()
         val classRef = db.collection("class").document(classId)
 
         db.runTransaction { transaction ->
             val snapshot = transaction.get(classRef)
-            val currentCapacity = snapshot.getLong("capacity") ?: 0
-            val newCapacity = currentCapacity + 1
-            transaction.update(classRef, "capacity", newCapacity)
-            null
+            val currentCapacity = snapshot.getLong("capacity") ?: 0 // Obtiene el valor actual del campo 'capacity' o usa 0 si es nulo
+            val newCapacity = currentCapacity + 1 // Incrementa el valor de 'capacity' en 1
+            transaction.update(classRef, "capacity", newCapacity) // Actualiza el campo 'capacity' en la transacción
+            null // Indica que no se necesita devolver ningún resultado
         }.addOnSuccessListener {
             Toast.makeText(requireContext(), "Campo 'capacity' incrementado correctamente", Toast.LENGTH_SHORT).show()
         }.addOnFailureListener { exception ->
             Toast.makeText(requireContext(), "Error al incrementar el campo 'capacity'", Toast.LENGTH_SHORT).show()
         }
-
-
     }
+
+
+    /**
+     * Permite convertir la primera letra de una cadena en mayucula
+     */
     fun capitalizeFirstLetter(input: String): String {
         if (input.isEmpty()) {
-            return input
+            return input // Si la cadena está vacía, no se hace ningún cambio y se devuelve tal cual
         }
 
-        val firstChar = input.substring(0, 1)
-        val remainingChars = input.substring(1)
+        val firstChar = input.substring(0, 1) // Obtiene el primer carácter de la cadena
+        val remainingChars = input.substring(1) // Obtiene el resto de la cadena
 
-        return "${firstChar.lowercase(Locale.getDefault())}$remainingChars"
+        // Concatena el primer carácter en mayúscula con el resto de la cadena y lo devuelve
+        return "${firstChar.uppercase(Locale.getDefault())}$remainingChars"
     }
 
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/urbanfit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/urbanfit/ui/home/HomeFragment.kt
@@ -1,13 +1,30 @@
 package com.example.urbanfit.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import com.example.urbanfit.BookingGYM
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.urbanfit.MainActivity
 import com.example.urbanfit.databinding.FragmentHomeBinding
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Calendar
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import android.graphics.Color
+import androidx.lifecycle.MutableLiveData
+import java.util.Date
+
 
 class HomeFragment : Fragment() {
 
@@ -16,7 +33,9 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    lateinit var email: String
+    private lateinit var db: FirebaseFirestore
+    private lateinit var chart: BarChart
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,16 +46,279 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        fixArray()
+        chart = binding.chart
+        countReservationsCurrentWeek(email)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+
+
+
         return root
     }
+    /**
+     * Inicia la obtencion de datos para la lista
+     * */
+    private fun fixArray() {
+        email = (activity as MainActivity).email // Obtiene el valor de la variable "email" desde la actividad contenedora (MainActivity)
+        db = FirebaseFirestore.getInstance() // Obtiene una instancia de FirebaseFirestore
+
+        getBookings() // Llama a la función "getBookings" para obtener las reservas
+    }
+    private fun getBookings() {
+
+        val userRef = db.collection("user").document(email).collection("booking")
+
+        val today = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0) // Establecer la hora en 00:00
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.time
+
+        val query = userRef.whereEqualTo("date", today)
+
+        val bookingList = mutableListOf<BookingGYM>() // Lista para almacenar las reservas
+
+        query.get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot) {
+                    // Acceder a los datos de cada documento
+                    val hour = document.getString("hour") ?: ""
+                    val date = convertTimestampToCalendar(document.getTimestamp("date") ?: Timestamp.now())
+                    val className = document.getString("class") ?: ""
+                    val associatedGym = document.getString("associatedGym") ?: ""
+
+                    // Crear objeto Booking y agregarlo a la lista
+                    val booking = BookingGYM(hour, date, className, associatedGym)
+                    bookingList.add(booking)
+
+                    // Realizar las acciones deseadas con los datos
+
+                    Log.d("Firestore", "Hora: $hour, Fecha: $date, Clase: $className, Gimnasio: $associatedGym")
+                }
+
+
+
+            }
+            .addOnFailureListener { exception ->
+                // Manejar el error en caso de que ocurra
+                Toast.makeText(requireContext(), "Error al obtener los documentos: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+    /**
+     * Convierte un objeto Timestamp a un objeto Calendar
+     * */
+    fun convertTimestampToCalendar(timestamp: Timestamp): Calendar {
+        val date = timestamp.toDate() // Convierte el objeto Timestamp a un objeto Date
+        return Calendar.getInstance().apply { time = date } // Asigna la fecha al objeto Calendar y lo devuelve
+    }
+    /**
+     *  Establece configuracion del estilo y las propiedades del char.
+     * */
+    private fun setupChart() {
+        // Configura el estilo y las propiedades del gráfico
+        chart.description.isEnabled = false
+        chart.setDrawValueAboveBar(true)
+
+        val xAxis = chart.xAxis
+        xAxis.position = XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(true)
+
+        chart.axisLeft.setDrawGridLines(false)
+        chart.axisRight.setDrawGridLines(false)
+
+        chart.animateY(800)
+    }
+    /**
+     * Establece configuracion de lineas y margenes de char.
+     * */
+    private fun setChartMargins() {
+        chart.setExtraOffsets(0f, 0f, 0f, 0f)  // Establece los márgenes superior, izquierdo, inferior y derecho a 0
+
+        val xAxis = chart.xAxis
+        xAxis.setDrawAxisLine(false)  // No dibujar la línea del eje X
+        xAxis.setDrawGridLines(false)  // No dibujar las líneas de la cuadrícula del eje X
+        xAxis.setDrawLabels(true)  // Mostrar los labels del eje X
+
+        val leftAxis = chart.axisLeft
+        leftAxis.setDrawAxisLine(false)  // No dibujar la línea del eje Y izquierdo
+        leftAxis.setDrawGridLines(false)  // No dibujar las líneas de la cuadrícula del eje Y izquierdo
+        leftAxis.setDrawLabels(false)  // No mostrar los labels del eje Y izquierdo
+
+        val rightAxis = chart.axisRight
+        rightAxis.setDrawAxisLine(false)  // No dibujar la línea del eje Y derecho
+        rightAxis.setDrawGridLines(false)  // No dibujar las líneas de la cuadrícula del eje Y derecho
+        rightAxis.setDrawLabels(false)  // No mostrar los labels del eje Y derecho
+
+        chart.legend.isEnabled = false  // Ocultar la leyenda
+
+        chart.invalidate()  // Actualizar el gráfico
+    }
+
+    /**
+     * Establece los datos y algunas configuraciones del char
+     */
+    private fun setData(daysOfWeek: List<Float>) {
+        val bookingsPerDay = listOf("L", "M", "MX", "J", "V", "S", "D") // Lista de los nombres de los días de la semana
+
+        val entries = mutableListOf<BarEntry>()
+        for (i in daysOfWeek.indices) {
+            val bookingCount = daysOfWeek[i]
+            entries.add(BarEntry(i.toFloat(), bookingCount)) // Agregar las entradas de datos para cada día de la semana
+        }
+
+        val colors = listOf(
+            Color.parseColor("#FF0000"), // Rojo
+            Color.parseColor("#00FF00")  // Verde
+        )
+
+        val dataSet = BarDataSet(entries, "Reservas") // Crear un conjunto de datos de barras con las entradas
+        dataSet.setColors(colors) // Asignar colores a las barras
+        dataSet.setDrawValues(false) // No mostrar los valores en las barras
+
+        val barData = BarData(dataSet) // Crear los datos del gráfico de barras
+        barData.barWidth = 0.9f // Ajustar el ancho de la barra
+
+        chart.data = barData // Asignar los datos al gráfico
+        chart.invalidate() // Invalidar el gráfico para que se actualice y muestre los cambios
+
+        // Configurar los labels en el eje X
+        chart.setExtraOffsets(0f, 0f, 0f, 20f) // Ajustar el margen inferior del gráfico
+        val xAxis = chart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(bookingsPerDay) // Asignar los nombres de los días como etiquetas en el eje X
+        xAxis.position = XAxis.XAxisPosition.BOTTOM // Posicionar el eje X en la parte inferior
+        xAxis.granularity = 1f // Configurar la separación entre las etiquetas en el eje X
+        xAxis.setDrawAxisLine(true) // Mostrar la línea del eje X
+        xAxis.setDrawGridLines(false) // No mostrar las líneas de la cuadrícula en el eje X
+        xAxis.setDrawLabels(true) // Mostrar las etiquetas en el eje X
+        xAxis.textSize = 26f // Tamaño de la letra en el eje X
+        // Ajustar el tamaño de la letra en el eje Y
+
+        // Aquí puedes agregar cualquier configuración adicional que necesites para el gráfico
+    }
+
+    /**
+     * Permite recibir la lista producida con las reservas de la semana
+     * y adaptarlas para meterlas en el char
+     * */
+    fun compareAndAssign(values: MutableLiveData<List<Int>>): List<Float> {
+        val currentValue = values.value ?: emptyList() // Obtener el valor actual de MutableLiveData. Si es nulo, se utiliza una lista vacía.
+
+        return currentValue.map { if (it > 0) 2.0f else 0.0f } // Mapear cada valor de la lista actual. Si es mayor que 0, se asigna 2.0f, de lo contrario, se asigna 0.0f.
+    }
+
+
+    fun countReservationsCurrentWeek(userId: String) {
+        val firestore = FirebaseFirestore.getInstance()
+        val startOfWeek = getStartOfWeek() // Obtener la fecha de inicio de la semana actual
+        val endOfWeek = getEndOfWeek() // Obtener la fecha de fin de la semana actual
+
+        val dayNames = listOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
+
+        val bookingCounts = MutableList(dayNames.size) { 0 } // Lista mutable para almacenar los conteos de reserva por día
+
+        val bookingCountsLiveData = MutableLiveData<List<Int>>() // LiveData para almacenar los conteos de reserva
+
+        firestore.collection("user")
+            .document(userId)
+            .collection("booking")
+            .whereGreaterThanOrEqualTo("date", startOfWeek)
+            .whereLessThanOrEqualTo("date", endOfWeek)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot.documents) {
+                    val bookingDate = document.getDate("date")
+                    val dayOfWeek = getDayOfWeek(bookingDate)
+                    if (dayOfWeek != null) {
+                        bookingCounts[dayOfWeek - 1]++ // Incrementar el conteo de reserva para el día correspondiente
+                    }
+                }
+                bookingCountsLiveData.value = bookingCounts // Asignar los conteos de reserva al LiveData
+
+                // Imprimir el valor actual de bookingCountsLiveData
+                println("bookingCountsLiveData: ${bookingCountsLiveData.value}")
+
+                // Obtener y procesar los valores comparados y asignados
+                val data = compareAndAssign(bookingCountsLiveData)
+                println("Resultado: $data")
+
+                setChartMargins() // Configurar los márgenes del gráfico
+                setupChart() // Configurar el gráfico
+                setData(data) // Establecer los datos en el gráfico
+            }
+            .addOnFailureListener { exception ->
+                // Manejar cualquier error que ocurra al obtener las reservas
+                println("Error al obtener las reservas: ${exception.message}")
+            }
+    }
+
+    /**
+     * Toma una fecha (Date) como parámetro y devuelve el día de la
+     * semana correspondiente como un valor entero estableciendo
+     * el dia en el que comienza la semana.
+     * */
+    fun getDayOfWeek(date: Date?): Int? {
+        if (date != null) {
+            val calendar = Calendar.getInstance() // Obtener una instancia del objeto Calendar
+            calendar.time = date // Establecer la fecha en el calendario
+            var dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) // Obtener el día de la semana como un valor entero
+
+            if (dayOfWeek == Calendar.SUNDAY) {
+                dayOfWeek = 7 // Cambiar el índice del domingo a 7 (último día de la semana)
+            } else {
+                dayOfWeek -= 1 // Restar 1 para ajustar el índice a partir de 0
+            }
+
+            return dayOfWeek // Devolver el día de la semana
+        }
+        return null // Si la fecha es nula, se devuelve nulo
+    }
+
+    /**
+     * Obtiene la fecha de incio de la semana
+     * */
+    fun getStartOfWeek(): Date {
+        val calendar = Calendar.getInstance() // Obtener una instancia del objeto Calendar
+
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek) // Establecer el primer día de la semana
+        calendar.add(Calendar.DAY_OF_WEEK, -6) // Restar 6 días para obtener el inicio de la semana actual
+
+        // Establecer la hora, minutos, segundos y milisegundos a cero para obtener la fecha de inicio exacta
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        println("Inicio de la semana: ${calendar.time}")
+
+        return calendar.time // Devolver la fecha de inicio de la semana como un objeto Date
+    }
+
+    /**
+     * Obtiene la fecha de finalizacion de la semana
+     * */
+    fun getEndOfWeek(): Date {
+        val calendar = Calendar.getInstance() // Obtener una instancia del objeto Calendar
+
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek) // Establecer el primer día de la semana
+        calendar.set(Calendar.HOUR_OF_DAY, 23) // Establecer la última hora del día
+        calendar.set(Calendar.MINUTE, 59) // Establecer el último minuto de la hora
+        calendar.set(Calendar.SECOND, 59) // Establecer el último segundo del minuto
+        calendar.set(Calendar.MILLISECOND, 999) // Establecer el último milisegundo del segundo
+
+        println("Fin de la semana: ${calendar.time}")
+
+        return calendar.time // Devolver la fecha de fin de la semana como un objeto Date
+    }
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
