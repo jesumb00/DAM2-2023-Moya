9504f2f5f7f81ce2c8a541bc13cbc7b56e510c77
Jesús <jesus.moya.barajas00@gmail.com>
2023-05-20 19:23:47 +0200

    Guardando subcoleccion y luego recuperarla para probar lo que se va ahcer en inicio

--- app/src/main/java/com/example/urbanfit/ui/bookings/BookingsFragment.kt
+++ app/src/main/java/com/example/urbanfit/ui/bookings/BookingsFragment.kt
@@ -14,6 +14,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 
 
+
+
 class BookingsFragment : Fragment(), AdapterCallback {
 
     private var _binding: FragmentBookingsBinding? = null
@@ -34,9 +36,6 @@ class BookingsFragment : Fragment(), AdapterCallback {
         val root: View = binding.root
 
         fixArray()
-
-
-
 
         return root
     }
@@ -77,7 +76,8 @@ class BookingsFragment : Fragment(), AdapterCallback {
                     val currentHour = currentTime.get(Calendar.HOUR_OF_DAY)
                     val currentMinute = currentTime.get(Calendar.MINUTE)
 
-                    //if (classHour > currentHour || (classHour == currentHour && classMinute > currentMinute)) {
+                    if (classHour > currentHour || (classHour == currentHour && classMinute > currentMinute)) {
+                        val id = document.id
                         val name = document.getString("name") ?: ""
                         val description = document.getString("description") ?: ""
                         val schedule = document.getString("schedule") ?: ""
@@ -87,6 +87,7 @@ class BookingsFragment : Fragment(), AdapterCallback {
                         val monitor = document.getString("monitor") ?: ""
 
                         tempList += ClassGym(
+                            id = id,
                             name = name,
                             description = description,
                             schedule = schedule,
@@ -96,7 +97,7 @@ class BookingsFragment : Fragment(), AdapterCallback {
                             associatedGym = associatedGym,
                             monitor = monitor
                         )
-                    //}
+                    }
                 }
             }
 
@@ -119,22 +120,86 @@ class BookingsFragment : Fragment(), AdapterCallback {
 
     override fun onItemClicked(data: ClassGym) {
         var name=capitalizeFirstLetter(data.name)
-        Toast.makeText(requireContext(),"$name",Toast.LENGTH_SHORT).show()
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Confirmación")
         builder.setMessage("¿Deseas reservar la $name?")
         builder.setPositiveButton("Reservar") { dialog, which ->
             // Acciones a realizar cuando se presiona el botón Aceptar
-            Toast.makeText(requireContext(), "Aceptar", Toast.LENGTH_SHORT).show()
+            db = FirebaseFirestore.getInstance()
+
+            val currentDate = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 0) // Establecer la hora en 00:00
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }.time
+            val userRef = db.collection("user").document(email).collection("booking")
+            val subcollectionData = hashMapOf(
+                "hour" to data.schedule,
+                "date" to currentDate,
+                "class" to data.name,
+                "associatedGym" to data.associatedGym
+            )
+            val query = userRef.whereEqualTo("date", currentDate)
+                .whereEqualTo("class", data.name)
+                .whereEqualTo("associatedGym", data.associatedGym)
+
+            query.get()
+                .addOnSuccessListener { documents ->
+                    if (!documents.isEmpty) {
+                        // Ya existe una reserva para la fecha de hoy y coinciden todos los datos
+                        seeMessageRepeatReservationShow("Ya tienes una reserva para esta clase")
+                    } else {
+                        // No hay reserva existente, agregar los datos a la subcolección
+                        userRef.add(subcollectionData)
+                            .addOnSuccessListener { documentReference ->
+                                seeMessageRepeatReservationShow("Se añadió la reserva")
+                                incrementCapacity(data.id)
+                            }
+                            .addOnFailureListener { exception ->
+                                seeMessageRepeatReservationShow("No se pudo añadir la reserva")
+                            }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    seeMessageRepeatReservationShow("Error al consultar las reservas")
+                }
         }
 
-        builder.setNegativeButton("Cancelar") { dialog, which ->
-            // Acciones a realizar cuando se presiona el botón Cancelar
-            Toast.makeText(requireContext(), "Cancelar", Toast.LENGTH_SHORT).show()
-        }
+        builder.setNegativeButton("Cancelar", null)
 
         val dialog = builder.create()
         dialog.show()
+    }
+    private fun seeMessageRepeatReservationShow(message : String){
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Aviso")
+        builder.setMessage(message)
+
+// Configurar el botón de aceptar
+        builder.setNegativeButton("Aceptar", null)
+
+// Crear y mostrar la ventana emergente
+        val dialog = builder.create()
+        dialog.show()
+
+    }
+    fun incrementCapacity(classId: String) {
+        Toast.makeText(requireContext(), "$classId", Toast.LENGTH_SHORT).show()
+        val classRef = db.collection("class").document(classId)
+
+        db.runTransaction { transaction ->
+            val snapshot = transaction.get(classRef)
+            val currentCapacity = snapshot.getLong("capacity") ?: 0
+            val newCapacity = currentCapacity + 1
+            transaction.update(classRef, "capacity", newCapacity)
+            null
+        }.addOnSuccessListener {
+            Toast.makeText(requireContext(), "Campo 'capacity' incrementado correctamente", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener { exception ->
+            Toast.makeText(requireContext(), "Error al incrementar el campo 'capacity'", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
     fun capitalizeFirstLetter(input: String): String {
